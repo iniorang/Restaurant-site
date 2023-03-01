1975819cf8cd01f10167396bbf474adbe863240c
ini_orang <freemiumlemper300@gmail.com>
2023-03-01 20:43:06 +0700

    Updating

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const navEl
+const navEl = document.querySelector('.navbar');
+window.addEventListener('scroll',()=>{
+    if(window.scrollY >= 56){
+        navEl.classList.add('navbar-scrolled');
+    }else if(window.scrollY < 56){
+        navEl.classList.remove('navbar-scrolled');
+    }
+});
