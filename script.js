0deea6c36ecab9fa8f64b448db0871784824364d
ini_orang <freemiumlemper300@gmail.com>
2023-06-03 16:10:40 +0700

    Small Change

--- script.js
+++
@@ -1,9 +0,0 @@
-const navEl = document.querySelector('.navbar');
-window.addEventListener('scroll',()=>{
-    if(window.scrollY >= 56){
-        navEl.classList.add('navbar-scrolled');
-    }else if(window.scrollY < 56){
-        navEl.classList.remove('navbar-scrolled');
-    }
-});
-"https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"
